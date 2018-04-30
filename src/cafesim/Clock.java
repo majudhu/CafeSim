df9077c028a5dea77a32701ecbba397a2124d2b9
Majudhu <majudhu.sup@gmail.com>
2018-04-30 11:10:24 +0800

    Use microseconds

--- src/cafesim/Clock.java
+++ src/cafesim/Clock.java
@@ -1,21 +1,31 @@
 package cafesim;
 
 public class Clock {
+	static final long openTime = 1000;
 	Cafe cafe;
 	private long startTime;
 	
 	public Clock(Cafe cafe) {
 		this.cafe = cafe;
-		startTime = System.currentTimeMillis();
+		startTime = System.nanoTime()/1000;
 	}
 
-	public long getTime() {
-		return System.currentTimeMillis() - startTime;
+	public long getTimeElapsed() {
+		return System.nanoTime()/1000 - startTime;
+	}
+	
+	public long getTimeLeft() {
+		return openTime - getTimeElapsed();
+	}
+	
+	public String getTimeString() {
+		long t = getTimeElapsed();
+		return String.format("%02d.%02d.%02d", ((t/60/60)%60), ((t/60)%60), (t%60));
 	}
 
 
 	public void log(String name, String info) {
-		System.out.println("Thread-" + name + ": " + getTime() + ": " + ": " + info);
+		System.out.println("Thread-" + name + ": " + getTimeString() + ": " + info);
 	}
 
 }
